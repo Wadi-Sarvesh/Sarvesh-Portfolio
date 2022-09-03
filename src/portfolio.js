72f658ab96ea3a4b3b0ad324e91bc82e05371ac4
Sarvesh Wadi <wadisarvesh33@gmail.com>
2022-09-04 01:50:42 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -262,7 +262,7 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
