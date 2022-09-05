37a82b92c98b32a54f0739e2583ff3da7095eda8
Wadi-Sarvesh <Wadi-Sarvesh@users.noreply.github.com>
2022-09-05 12:13:42 +0000

    Deploying to gh-pages from  @ dfbc6f624d02c219b014c579cb5dec84367fcd8f 🚀

---
+++ precache-manifest.6e6fad103336763b90844f221cd93314.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262bdbceaf6df9ae22e90d852eac4040",
+    "url": "/Sarvesh-Portfolio/index.html"
+  },
+  {
+    "revision": "99643b771ff5ee86fbd6",
+    "url": "/Sarvesh-Portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "ad4479fd4c9f36bbc5c0",
+    "url": "/Sarvesh-Portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "11b4369834fbff91ba3b",
+    "url": "/Sarvesh-Portfolio/static/css/main.142bbe9c.chunk.css"
+  },
+  {
+    "revision": "65f82133e01c1c28bb7f",
+    "url": "/Sarvesh-Portfolio/static/js/2.3f15c26d.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/Sarvesh-Portfolio/static/js/2.3f15c26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99643b771ff5ee86fbd6",
+    "url": "/Sarvesh-Portfolio/static/js/3.e8248f69.chunk.js"
+  },
+  {
+    "revision": "ad4479fd4c9f36bbc5c0",
+    "url": "/Sarvesh-Portfolio/static/js/4.5de810b4.chunk.js"
+  },
+  {
+    "revision": "11b4369834fbff91ba3b",
+    "url": "/Sarvesh-Portfolio/static/js/main.8e1d7183.chunk.js"
+  },
+  {
+    "revision": "651f27bfdb5a7e0765c4",
+    "url": "/Sarvesh-Portfolio/static/js/runtime-main.781bde0d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Sarvesh-Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "b8c5089e13bec0c08c031a2791de8e51",
+    "url": "/Sarvesh-Portfolio/static/media/Google meet.b8c5089e.png"
+  },
+  {
+    "revision": "1850ebb9a979c1de42cad49bdc6f2d5f",
+    "url": "/Sarvesh-Portfolio/static/media/MPdeo.1850ebb9.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Sarvesh-Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c1c797a32513f95d1ac6325b1fb24706",
+    "url": "/Sarvesh-Portfolio/static/media/P2P App.c1c797a3.png"
+  },
+  {
+    "revision": "c5c6a06989b10c9150a07e8a8d911d16",
+    "url": "/Sarvesh-Portfolio/static/media/SafeRoute.c5c6a069.jpg"
+  },
+  {
+    "revision": "8ee376525417d7e3402ca1d7a7a30826",
+    "url": "/Sarvesh-Portfolio/static/media/Snowflake.8ee37652.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Sarvesh-Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Sarvesh-Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "261c4288ccb178710757a92afec116c3",
+    "url": "/Sarvesh-Portfolio/static/media/hackathon.261c4288.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Sarvesh-Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Sarvesh-Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Sarvesh-Portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "c5a6dc735b480f8b9e3e7bd802841211",
+    "url": "/Sarvesh-Portfolio/static/media/trophy.c5a6dc73.png"
+  }
+]);
